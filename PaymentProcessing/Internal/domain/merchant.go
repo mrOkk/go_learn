9ba4a881a81c511f27ce3c81b250a061e48d9fb4
Oleg Koposov <xtrimsmile@gmail.com>
2026-08-17 23:10:00 +0300

    prepare something for data layer

---
+++ PaymentProcessing/Internal/domain/merchant.go
@@ -0,0 +1,8 @@
+﻿package domain
+
+type Merchant struct {
+	Id       string  `json:"id"`
+	Name     string  `json:"name"`
+	IsActive bool    `json:"is_active"`
+	Balance  float64 `json:"balance"`
+}
---
+++ PaymentProcessing/Internal/domain/transaction.go
@@ -0,0 +1,9 @@
+﻿package domain
+
+type Transaction struct {
+	ID         string  `json:"id"`
+	MerchantID string  `json:"merchant_id"`
+	Amount     float64 `json:"amount"`
+	Status     string  `json:"status"`
+	Timestamp  string  `json:"timestamp"`
+}
